db17cf62a898bdcb06422385885fb47f5ce03617
David Turner <dmturner@princeton.edu>
2021-09-10 10:11:41 -0400

    fix: remove deletion of data archive from script.
    This makes it easier to track if the dataset has been downloaded
    already or not so we can cache things on della.

--- notebooks/isc/download_data.sh
+++ notebooks/isc/download_data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Test to see if the extracted data is present, if not, download the archive. The python notebook handles extraction and
-# deleting of the archive for us.
-test ! -e brainiak-aperture-isc-data && wget -nc https://zenodo.org/record/4300904/files/brainiak-aperture-isc-data.tgz
+# Download the data, the python notebook handles extraction and
+wget -nc https://zenodo.org/record/4300904/files/brainiak-aperture-isc-data.tgz
