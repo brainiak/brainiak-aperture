6822ef5a34420622a5f1f197a3e98474d67e7bba
David Turner <dmturner@princeton.edu>
2021-09-10 10:06:05 -0400

    fix: download_data script for isc no clobber

--- notebooks/isc/download_data.sh
+++ notebooks/isc/download_data.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-wget -nc https://zenodo.org/record/4300904/files/brainiak-aperture-isc-data.tgz
+
+# Test to see if the extracted data is present, if not, download the archive. The python notebook handles extraction and
+# deleting of the archive for us.
+test ! -e brainiak-aperture-isc-data && wget -nc https://zenodo.org/record/4300904/files/brainiak-aperture-isc-data.tgz
